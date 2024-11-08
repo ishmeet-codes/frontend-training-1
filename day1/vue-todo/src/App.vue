0edcc69f5b1300ebc8ee5ebd9f5c4155ece945cb
ishmeet-codes <ishmeetsinghxa@gmail.com>
2024-11-08 23:44:50 +0530

    vue/vite app created

---
+++ day1/vue-todo/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <button @click="increement">Increement</button>
+    <p>{{data.count}}</p>
+</template>
+
+<script setup>
+    import {reactive } from "vue";
+    const data = reactive({
+        count:0
+    })
+    function increement(){
+        data.count++;
+    }
+
+</script>
